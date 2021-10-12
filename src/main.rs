3b8171134bd87336de60a55c11cbcac2c64c1a39
ZPTX4869 <2532827403@qq.com>
2021-10-12 21:05:34 +0800

    2021-10-12+

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut map = HashMap::new();
+
+    map.insert(String::from("Blue"), 10);
+    map.insert(String::from("Red"), 20);
+
+    println!("{}", "Hello World!")
+}
